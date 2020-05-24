bf803f4bc971a2a8cc6fa3fa9522963db48fab9a
sandeb <sandrine.debatty@gmail.com>
2020-05-24 19:51:38 +0200

    routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,6 +1,5 @@
 <?php
 
-// src/Controller/WildController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +10,7 @@ class WildController extends AbstractController
 {
     /**
      * @Route("/", name="base")
-    */
+     */
 
     public function index() :Response
     {
@@ -22,4 +21,26 @@ class WildController extends AbstractController
             'website' => 'Fu#k is the new home ',
         ]);
     }
+
+    /**
+     * @Route("wild/show/{slug}", requirements={"slug"="[a-z0-9-]+$"}, defaults={"slug"=null}, name="wild_show")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug) : Response
+    {
+        if ($slug != null) {
+            $slug = ucwords(str_replace('-', ' ', $slug));
+        } else {
+            $slug = "Aucune série sélectionnée, veuillez choisir une série";
+        }
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug
+        ]);
+    }
+
+
+
+
+
 }
