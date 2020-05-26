0d64d7300aa2fbeb56b7f24c47abda7201176ad3
sandeb <sandrine.debatty@gmail.com>
2020-05-26 18:27:45 +0200

    quete8

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Program;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,26 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
 {
+
     /**
-     * @Route("/wild", name="wild_index")
-     * @return Response A response instance
-     */
-    public function index(): Response
+    * @Route("/wild", name="wild_index")
+    */
+
+    public function index()
     {
-        $programs = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findAll();
-
-        if (!$programs) {
-            throw $this->createNotFoundException(
-                'No program found in program\'s table.'
-            );
-        }
-
-        return $this->render(
-            'wild/index.html.twig', [
-                'programs' => $programs
+        $programs = $this->getDoctrine()->getRepository(Program::class)->findAll();
+        return $this->render('wild/index.html.twig', [
+            'programs' => $programs,
         ]);
+
     }
 
     /**
@@ -39,18 +33,65 @@ class WildController extends AbstractController
      */
     public function show($slug) : Response
     {
-        if ($slug != null) {
-            $slug = ucwords(str_replace('-', ' ', $slug));
-        } else {
-            $slug = "Aucune série sélectionnée, veuillez choisir une série";
+        if (!$slug){
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
         }
-        return $this->render('wild/show.html.twig', [
-            'slug' => $slug
+        $slug = preg_replace(
+            '/-/',
+            ' ',ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig',[
+            'program'=> $program,
+            'slug' => $slug,
         ]);
+
+        /***************** quête_5 ********************************************/
+       // if ($slug != null) {
+         //   $slug = ucwords(str_replace('-', ' ', $slug));
+        //} else {
+          //  $slug = "Aucune série sélectionnée, veuillez choisir une série";
+        //}
+        //return $this->render('wild/show.html.twig', [
+            //'slug' => $slug
+        //]);
+        /********************************************************************/
     }
 
+    /**
+     * @Route("wild/category/{categoryName}", name="wild_category")
+     * @param $categoryName
+     * @return Response
+     */
 
+    public function showByCategory(string $categoryName) : Response
+    {
 
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name'=> mb_strtolower($categoryName)]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(['category' => $category], ['id'=> 'DESC'], 3);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No program with ' . $categoryName . ' title, found in program\'s table.');
+        }
+        return $this->render('wild/category.html.twig', [
+            'programs' => $programs,
+            'categoryName'  => $category
+        ]);
+
+    }
 
 
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
  */
@@ -16,26 +16,66 @@ class Category
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=255)
      */
     private $name;
-
+    /**
+     * @ORM\OneToMany(targetEntity=Program::class, mappedBy="category")
+     */
+    private $programs;
+    public function __construct()
+    {
+        $this->programs = new ArrayCollection();
+    }
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): self
     {
         $this->name = $name;
+        return $this;
+    }
 
+    /**
+     * Generates the magic method
+     *
+     */
+    public function __toString(){
+        // to show the name of the Category in the select
+        return $this->name;
+        // to show the id of the Category in the select
+        // return $this->id;
+    }
+    /**
+     * @return Collection|Program[]
+     */
+    public function getPrograms(): Collection
+    {
+        return $this->programs;
+    }
+    public function addProgram(Program $program): self
+    {
+        if (!$this->programs->contains($program)) {
+            $this->programs[] = $program;
+            $program->setCategory($this);
+        }
+        return $this;
+    }
+    public function removeProgram(Program $program): self
+    {
+        if ($this->programs->contains($program)) {
+            $this->programs->removeElement($program);
+            // set the owning side to null (unless already changed)
+            if ($program->getCategory() === $this) {
+                $program->setCategory(null);
+            }
+        }
         return $this;
     }
 }
